fcc5a43354d8fa0ab26fa78a7a1057f0f4ee7d10
lerwing <adminosb@gmail.com>
2022-06-04 17:37:40 +0300

    правка для сервера

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<MenuLeft/>}>
+        <Route path='/pokemons' element={<MenuLeft/>}>
           <Route index element={<Search/>}/>
           <Route path='collection' element={<Collection/>}/>
           <Route path='*' element={<Eror404/>}/>
